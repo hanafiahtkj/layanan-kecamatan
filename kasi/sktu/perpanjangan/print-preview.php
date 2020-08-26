f9782dbf74d1ff8c0eb6d85d9bf153a85c8bbd9f
G-MF <gusti.fahrubi@gmail.com>
2020-08-26 13:43:13 +0800

    perbaiki format cetak sktu

--- kasi/sktu/perpanjangan/print-preview.php
+++ kasi/sktu/perpanjangan/print-preview.php
@@ -42,16 +42,16 @@ $row  = $data->fetch_array();
 <body>
 
     <div class="kop">
-        <img src="../../../assets/img/logo-bjm.png" style="width: 90px; height: 130px; margin-bottom: -10px;">
+        <img src="../../../assets/img/logo-bjm.png" style="width: 90px; height: 125px; margin-bottom: -10px; margin-top: 8px;">
 
-        <p style="margin-top: -20px;">
-            <label style="font-weight: bold; font-size: 18;">
+        <p style="margin-top: -20px; margin-bottom: -8px;">
+            <label style="font-weight: bold; font-size: 17;">
                 PEMERINTAH KOTA BANJARMASIN <br>
             </label>
-            <label style="font-weight: bold; font-size: 22; line-height: 23px;">
+            <label style="font-weight: bold; font-size: 21; line-height: 23px;">
                 KECAMATAN BANJARMASIN UTARA <br>
             </label>
-            <label style="line-height: 1.2; font-size: 11;">
+            <label style="line-height: 1.2; font-size: 9;">
                 Jalan HKSN RT. 10 Alalak Utara Banjarmasin 70125<br>
                 <img src="../../../assets/img/phone.png" style="margin-top: 2px; width: 15px; height: 15px;"> (0511) 3306828
                 &nbsp;&nbsp;&nbsp;&nbsp;
@@ -63,13 +63,13 @@ $row  = $data->fetch_array();
 
     </div>
 
-    <p style="text-align: center; margin-top: 2%;">
+    <p style="text-align: center;">
         <label>
-            <b style="font-size: 16;"><u>SURAT KETERANGAN TEMPAT USAHA</u></b> <br>
-            <b style="font-size: 10;">Nomor</b> : <?= $row['nomor_sktu']; ?>
+            <b style="font-size: 15;"><u>SURAT KETERANGAN TEMPAT USAHA</u></b> <br>
+            <b style="font-size: 9;">Nomor : <?= $row['nomor_sktu']; ?></b>
         </label>
 
-        <table border="0" width="100%" style="text-align: left; font-size: 10;" cellpadding=" 1">
+        <table border="0" width="100%" style="text-align: left; font-size: 9; margin-top: -10px;" cellpadding=" 1">
             <tr style="vertical-align: top;">
                 <td width="13%">Membaca</td>
                 <td width="3%">:</td>
@@ -100,13 +100,13 @@ $row  = $data->fetch_array();
 
     </p>
 
-    <div style="text-align: center; font-size: 12;">
+    <div style="text-align: center; font-size: 11; margin-top: -15px; margin-bottom: -15px;">
         <u>MEMUTUSKAN</u>
     </div>
 
     <br>
 
-    <table border="0" width="100%" style="text-align: left; font-size: 10;" cellpadding=" 1">
+    <table border="0" width="100%" style="text-align: left; font-size: 9;" cellpadding=" 1">
         <tr style="vertical-align: top;">
             <td width="13%">Memberikan</td>
             <td width="3%">:</td>
@@ -115,54 +115,54 @@ $row  = $data->fetch_array();
         <tr style="vertical-align: top;">
             <td width="13%"></td>
             <td width="3%"></td>
-            <td width="40%">1. Nama Perusahaan</td>
+            <td width="35%">1. Nama Perusahaan</td>
             <td style="text-align: center;">:</td>
             <td width="60%"><b><?= strtoupper($row['nama_perusahaan']); ?></b></td>
         </tr>
         <tr style="vertical-align: top;">
             <td width="13%"></td>
             <td width="3%"></td>
-            <td width="40%">2. Alamat Perusahaan</td>
+            <td width="35%">2. Alamat Perusahaan</td>
             <td style="text-align: center;">:</td>
             <td width="60%"><?= strtoupper($row['alamat_perusahaan']); ?></td>
         </tr>
         <tr style="vertical-align: top;">
             <td width="13%"></td>
             <td width="3%"></td>
-            <td width="40%">3. Nama Pimpinan Perusahaan</td>
+            <td width="35%">3. Nama Pimpinan Perusahaan</td>
             <td style="text-align: center;">:</td>
             <td width="60%"><b><?= strtoupper($row['nama_pimpinan_perusahaan']); ?></b></td>
         </tr>
         <tr style="vertical-align: top;">
             <td width="13%"></td>
             <td width="3%"></td>
-            <td width="40%">4. Alamat Kediaman</td>
+            <td width="35%">4. Alamat Kediaman</td>
             <td style="text-align: center;">:</td>
             <td width="60%"><?= strtoupper($row['alamat_kediaman']); ?></td>
         </tr>
         <tr style="vertical-align: top;">
             <td width="13%"></td>
             <td width="3%"></td>
-            <td width="40%">5. Kegiatan Usaha</td>
+            <td width="35%">5. Kegiatan Usaha</td>
             <td style="text-align: center;">:</td>
             <td width="60%"><b><?= strtoupper($row['kegiatan_usaha']); ?></b></td>
         </tr>
         <tr style="vertical-align: top;">
             <td width="13%"></td>
             <td width="3%"></td>
-            <td width="40%">6. Masa Berlaku</td>
+            <td width="35%">6. Masa Berlaku</td>
             <td style="text-align: center;">:</td>
             <td width="60%">
                 <?=
-                    tgl_indo($row['masa_berlaku_awal']) . " S/D " .
-                        tgl_indo($row['masa_berlaku_akhir']);
+                    date('d', strtotime($row['masa_berlaku_awal'])) . " " . $bln[date('m', strtotime($row['masa_berlaku_awal']))] . " " . date('Y', strtotime($row['masa_berlaku_awal'])) . " S/D " .
+                        date('d', strtotime($row['masa_berlaku_akhir'])) . " " . $bln[date('m', strtotime($row['masa_berlaku_akhir']))] . " " . date('Y', strtotime($row['masa_berlaku_akhir']))
                 ?>
             </td>
         </tr>
         <tr style="vertical-align: top;">
             <td width="13%"></td>
             <td width="3%"></td>
-            <td width="40%">7. Sifat</td>
+            <td width="35%">7. Sifat</td>
             <td style="text-align: center;">:</td>
             <td width="60%"><b><?= strtoupper($row['sifat']); ?></b></td>
         </tr>
@@ -171,29 +171,30 @@ $row  = $data->fetch_array();
             <td width="13%"></td>
             <td width="3%"></td>
             <td colspan="3" align="justify">
-                <p>
-                    Surat Keterangan Tempat Usaha ini bukan merupakan Izin Usaha Perdagangan/Jasa, tetapi keterangan tentang Tempat Usaha/Kegiatan dari Perorangan / Badan Hukum sebagai dasar untuk pembuatan perizinan berikutnya
-                </p>
+
+                Surat Keterangan Tempat Usaha ini bukan merupakan Izin Usaha Perdagangan/Jasa, tetapi keterangan tentang Tempat Usaha/Kegiatan dari Perorangan / Badan Hukum sebagai dasar untuk pembuatan perizinan berikutnya
+
             </td>
         </tr>
     </table>
 
-    <table border="0" style="width: 100%; font-size: 10;">
+    <table border="0" style="width: 100%; font-size: 9;">
         <tr>
+            <td width="110%"></td>
             <td width="30%" align="right"> Dikeluarkan di</td>
             <td width="2%" align="center">:</td>
             <td width="30%">Banjarmasin</td>
         </tr>
         <tr>
+            <td width="110%"></td>
             <td width="30%" align="right">Pada Tanggal</td>
             <td width="2%" align="center">:</td>
             <td width="30%">
-                <?=
-                    tgl_indo($row['tgl_selesai']);
-                ?>
+                <?= tgl_indo(date('Y-m-d', strtotime($row['tgl_selesai']))); ?>
             </td>
         </tr>
         <tr style="text-align: center;">
+            <td width="110%"></td>
             <td colspan="3">
                 CAMAT,
                 <br><br><br><br><br>
@@ -217,7 +218,9 @@ use Dompdf\Dompdf;
 require_once "../../../assets/dompdf/autoload.inc.php";
 $dompdf = new Dompdf();
 $dompdf->loadHtml($html);
-$dompdf->setPaper('Legal', 'Portrait');
+// $dompdf->setPaper('legal', 'Portrait');
+$custompaper = array(0, 0, 609.4488, 935.433); // F4
+$dompdf->setPaper($custompaper, 'portrait');
 $dompdf->render();
-$dompdf->stream("sktu-perpanjangan.pdf", array("Attachment" => false));
+$dompdf->stream("sktu-baru.pdf", array("Attachment" => false));
 ?>
